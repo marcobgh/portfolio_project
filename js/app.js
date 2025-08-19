9a79ca9c7946cb7438a39aeac8c59864a0e40fca
Marco <maborghetti@outlook.com>
2025-08-19 17:05:32 -0300

    changing the navigation bar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const collapsibles = document.querySelectorAll(".collapsible");
+
 collapsibles.forEach((item) =>
-    item.addEventListener("click", function () {
-        this.classList.toggle("collapsible--expanded");
+    item.addEventListener("click", function (event) {
+        // só expande se o clique for no elemento com a classe .nav-toggler
+        if (event.target.closest(".nav-toggler")) {
+            this.classList.toggle("collapsible--expanded");
+        }
     })
 );
